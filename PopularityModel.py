8800c83541fbe02c546dacfaf5273b43a08b9a52
Huynh Chau <48887152+ChauHuynh1@users.noreply.github.com>
2022-05-04 15:01:14 +0700

    update long tail plot

--- PopularityModel.py
+++ PopularityModel.py
@@ -8,4 +8,4 @@ import numpy
 def country_popularity(data, country):
     country_recsys = data[data['Country'].isin(country)]
     country_recsys['Unique_ISBN'].value_counts()[:10]
-    print(country_recsys['Unique_ISBN'].value_counts()[:10])
+    print(country_recsys['Book_Title'].value_counts()[:10])
